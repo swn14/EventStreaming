0a62eb3e17645aa4b426a5cf2fd6f4b1fac29cc1
Sheldon Nofer <swnofer@gmail.com>
2023-03-05 21:25:01 -0500

    first commit

---
+++ src/Swn14.Domain.Profile/Program.cs
@@ -0,0 +1,10 @@
+using Swn14.Domain.Profile;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
---
+++ src/Swn14.Domain.User/Program.cs
@@ -0,0 +1,10 @@
+using Swn14.Domain.User;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
